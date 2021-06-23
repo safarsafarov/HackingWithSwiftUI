ab5e3575edb8a695a2df6095508f851557cd824d
safarsafarov <safarov.safar@hotmail.com>
2021-06-23 21:50:10 +0800

    commit

--- day-9/Day-9/Day-9/main.swift
+++ day-9/Day-9/Day-9/main.swift
@@ -4,18 +4,29 @@
 //
 //  Created by codestorm on 2021/7/1.
 //
+// Initializers
+//import Foundation
+//
+//struct User {
+//    var username: String
+//
+//    init() {
+//        username = "Anonymous"
+//        print("Creating a new user!")
+//
+//    }
+//}
+//
+//var user = User()
+//user.username = "twostraws"
 
-import Foundation
 
-struct User {
-    var username: String
-
-    init() {
-        username = "Anonymous"
-        print("Creating a new user!")
-        
-    }
-}
-
-var user = User()
-user.username = "twostraws"
+//Referring to the current instance
+//struct Person {
+//    var name: String
+//
+//    init(name: String) {
+//        print("\(name) was born!")
+//        self.name = name
+//    }
+//}
