93434c18a8cadf4e4c950bb8cbca8eb4312a863c
safarsafarov <safarov.safar@hotmail.com>
2021-06-24 21:56:05 +0800

    commit

--- day-9/Day-9/Day-9/main.swift
+++ day-9/Day-9/Day-9/main.swift
@@ -51,14 +51,15 @@ import Foundation
 //}
 
 // Static properties and methods
-
 struct Student {
+    static var classSize = 0
     var name: String
 
     init(name: String) {
         self.name = name
+        Student.classSize += 1
     }
 }
-
 let ed = Student(name: "Ed")
 let taylor = Student(name: "Taylor")
+
