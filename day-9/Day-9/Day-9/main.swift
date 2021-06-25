cb612d8e4b202a7d395f18d9fa4464098048f61d
safarsafarov <safarov.safar@hotmail.com>
2021-06-25 21:57:19 +0800

    commit

--- day-9/Day-9/Day-9/main.swift
+++ day-9/Day-9/Day-9/main.swift
@@ -51,15 +51,30 @@ import Foundation
 //}
 
 // Static properties and methods
-struct Student {
-    static var classSize = 0
-    var name: String
+//struct Student {
+//    static var classSize = 0
+//    var name: String
+//
+//    init(name: String) {
+//        self.name = name
+//        Student.classSize += 1
+//    }
+//}
+//let ed = Student(name: "Ed")
+//let taylor = Student(name: "Taylor")
+//
+//print(Student.classSize)
 
-    init(name: String) {
-        self.name = name
-        Student.classSize += 1
+
+//Access control
+struct Person {
+    private var id: String
+
+    init(id: String) {
+        self.id = id
+    }
+
+    func identify() -> String {
+        return "My social security number is \(id)"
     }
 }
-let ed = Student(name: "Ed")
-let taylor = Student(name: "Taylor")
-
