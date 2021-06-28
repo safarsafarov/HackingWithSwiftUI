a693ad5b03622d24df03b6f22500698fa1186341
safarsafarov <safarov.safar@hotmail.com>
2021-06-28 22:00:02 +0800

    commit

--- day-9/Day-9/Day-9/main.swift
+++ day-9/Day-9/Day-9/main.swift
@@ -81,8 +81,22 @@ import Foundation
 
 
 
-
-// Static properties and methods
+//All the properties and methods we’ve created so far have belonged to individual instances of structs, which means that if we had a Student struct we could create several student instances each with their own properties and methods:
+//
+//struct Student {
+//    var name: String
+//
+//    init(name: String) {
+//        self.name = name
+//    }
+//}
+//
+//let ed = Student(name: "Ed")
+//let taylor = Student(name: "Taylor")
+//You can also ask Swift to share specific properties and methods across all instances of the struct by declaring them as static.
+//
+//To try this out, we’re going to add a static property to the Student struct to store how many students are in the class. Each time we create a new student, we’ll add one to it:
+//
 //struct Student {
 //    static var classSize = 0
 //    var name: String
@@ -92,8 +106,7 @@ import Foundation
 //        Student.classSize += 1
 //    }
 //}
-//let ed = Student(name: "Ed")
-//let taylor = Student(name: "Taylor")
+//Because the classSize property belongs to the struct itself rather than instances of the struct, we need to read it using Student.classSize:
 //
 //print(Student.classSize)
 
